2ee8357642dfa7d0af8d56f2a6d9e2d9fd407f5d
thanhtoan15 <truongtoan241@gmail.com>
2022-12-18 18:15:06 +0700

    New

---
+++ src/main/java/javaOracleWebix/controller/Userpi.java
@@ -0,0 +1,23 @@
+package javaOracleWebix.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javaOracleWebix.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/user")
+public class Userpi {
+	@Autowired
+	private UserService service;
+    @GetMapping("/getAllUser")
+    ResponseEntity<?> getAllKhachHang()
+    {
+    	return ResponseEntity.ok(service.getAllUser());
+    }
+}
---
+++ src/test/java/javaOracleWebix/F4JavaOracleWebixGradle1ApplicationTests.java
@@ -0,0 +1,13 @@
+package javaOracleWebix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F4JavaOracleWebixGradle1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
